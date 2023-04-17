134bd88397e62a59f40474bbae340e079b3ffd70
AlexLiu333 <59188574+AlexLiu333@users.noreply.github.com>
2023-04-17 04:33:58 -0400

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -6,7 +6,7 @@ export default siteConfig({
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: 'd6494ee7dd6642b3ad400e91f40b3ee1',
+  rootNotionSpaceId: null,
 
   // basic site info (required)
   name: 'BoolG',
